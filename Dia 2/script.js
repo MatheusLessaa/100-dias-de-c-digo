2e80b6ce6dc300508deaf36719ef13ea98fe9027
MatheusLessaa <m.lessa2310@gmail.com>
2024-09-05 14:55:27 -0300

    Dia 2

---
+++ Dia 2/script.js
@@ -0,0 +1,9 @@
+let user1 = prompt ("Digite o primeiro número de usuário");
+let user2 = prompt ("Digite o segundo número de usuário");
+
+user1 = parseFloat(user1);
+user2 = parseFloat(user2);
+
+let soma = user1+user2;
+alert("Resultado da soma: "+soma);
+console.log("Resultado da soma: ", soma);

7590bf6dd67b02bc5c4b013d273fd14ddbd4bb05
MatheusLessaa <m.lessa2310@gmail.com>
2024-09-05 22:05:08 -0300

    Dia 2

---
+++ Dia 2/main.py
@@ -0,0 +1,5 @@
+user1 = float(input("Digite o primeiro número de usuário: "))
+user2 = float(input("Digite o segundo número de usuário: "))
+
+result = user1 + user2
+print("o resultado é: ",result)
